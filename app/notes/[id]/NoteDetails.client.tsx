cdb6f3ad65861abc3b3c5e696c81ee7722cffd8f
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-03-06 18:02:11 +0100

    upd

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import css from './NoteDetails.module.css';
+import css from './NoteDetails.client.module.css';
 import EmptyState from '@/components/EmptyState/EmptyState';
 
 interface NoteDetailsClientProps {
